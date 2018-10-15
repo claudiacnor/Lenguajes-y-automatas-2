1f9d2275781c6ccb4e51ac5d7773b8e77aaa9549
Karina romero <43820718+karr123@users.noreply.github.com>
2018-10-15 12:17:34 -0600

    validacion variables
    validacion de variables usadas y no definidas y variables usadas y no declaradas

--- src/lya/AnalisisSemantico.java
+++ src/lya/AnalisisSemantico.java
@@ -6,8 +6,7 @@ import java.util.HashMap;
 public class AnalisisSemantico {
 	ArrayList<String> linea = new ArrayList<String>();
 	private HashMap<String, TablaSimbolos> tablaSimbolos = new HashMap<String, TablaSimbolos>();
-
-	// private ArrayList<String> listaErroresSemanticos = new ArrayList<String>();
+	private ArrayList<String> listaErroresSemanticos = new ArrayList<String>();
 	/*
 	 * private ArrayList<String> listaErroresSemanticos; private ArrayList<String>
 	 * operadoresaritmeticos; private ArrayList<String> operadoreslogicos;
@@ -21,20 +20,20 @@ public class AnalisisSemantico {
 	public AnalisisSemantico(String codigo) {
 		recorreCodigo(codigo);
 		LlenaTabla();
-		System.out.println(tablaSimbolos.get("y").getValor());
+		System.out.println(listaErroresSemanticos.get(0));
+		System.out.println(listaErroresSemanticos.get(1));
 	}
-
 	public void recorreCodigo(String codigo) {
 		String parrafo = "";
 		for (int i = 0; i < codigo.length(); i++) {
 			if (codigo.charAt(i) == '{' || codigo.charAt(i) == '}' || codigo.charAt(i) == ';') {
 				linea.add(parrafo);
 				parrafo = "";
-			} else
+			}
+			else
 				parrafo += Character.toString(codigo.charAt(i));
 		}
 	}
-
 	public void LlenaTabla() {
 		String parrafo;
 		CharSequence sInt = "int", sIgual = "=";
@@ -43,16 +42,18 @@ public class AnalisisSemantico {
 		CharSequence sBool = "boolean";
 		for (int i = 0; i < linea.size(); i++) {
 			parrafo = linea.get(i);
-			if (parrafo.contains(sInt) || parrafo.contains(sDouble) || parrafo.contains(sBool)
-					|| parrafo.contains(sString)) {
+			if (parrafo.contains(sInt) || parrafo.contains(sDouble) || parrafo.contains(sBool) || parrafo.contains(sString)) {
 				AgregaVariable(parrafo, sInt, i + 1);
 				AgregaVariable(parrafo, sString, i + 1);
 				AgregaVariable(parrafo, sDouble, i + 1);
 				AgregaVariable(parrafo, sBool, i + 1);
+			} else {
+				if (parrafo.contains(sIgual)) {
+					Operaciones(parrafo, i + 1);
+				}
 			}
 		}
 	}
-
 	public void AgregaVariable(String parrafo, CharSequence Tipo, int pos) {
 		String parrafoAux = "", variable = "", valor = "";
 		CharSequence sIgual = "=", sPublic = "public", sPrivate = "private";
@@ -70,8 +71,37 @@ public class AnalisisSemantico {
 				}
 				parrafoAux += Character.toString(parrafo.charAt(j));
 			}
-
+			// Validar si la variable ya esta declarada
+			if (tablaSimbolos.containsKey(variable))
+				listaErroresSemanticos
+						.add("La variable " + "'" + variable + "'" + " ya se encuentra declarada en el renglon "
+								+ tablaSimbolos.get(variable).getPosicion() + ".");
+			else {
+				if (parrafo.contains(sPublic) || parrafo.contains(sPrivate))
+					tablaSimbolos.put(variable, new TablaSimbolos(Tipo.toString(), pos, valor, "global"));
+				else
+					tablaSimbolos.put(variable, new TablaSimbolos(Tipo.toString(), pos, valor, "local"));
+			}
 		}
 
+	}
+	public void Operaciones(String parrafo, int pos) {
+		String parrafoAux = "", variable = "";
+		// Variables usadas y no defindas
+		// Se recorre el parrafo para obtener la variable de la operación
+		for (int j = 0; j < parrafo.length(); j++) {
+			if (parrafo.charAt(j) == '=') {
+				variable = parrafoAux;
+				break;
+			} else { //se eliminan los espacios en blanco
+				if (!Character.isWhitespace(parrafo.charAt(j))) {
+					parrafoAux += Character.toString(parrafo.charAt(j));
+				}
+			}
+		}
+		if (!tablaSimbolos.containsKey(variable)) {
+			listaErroresSemanticos
+					.add("La variable " + "'" + variable + "' en la posición " + pos + " no ha sido definida.");
+		}
 	}
 }
