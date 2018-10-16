032119e01f9809e0307e55edcb4c01d9428960b8
Manuel Villegas <HumbaPumbaMV@gmail.com>
2018-10-15 20:39:08 -0600

    Agregado verificador de asignaciones a variables (BUG) Arreglar parte de operandos

--- src/lya/AnalisisSemantico.java
+++ src/lya/AnalisisSemantico.java
@@ -7,12 +7,13 @@ public class AnalisisSemantico {
 	ArrayList<String> linea = new ArrayList<String>();
 	private HashMap<String, TablaSimbolos> tablaSimbolos = new HashMap<String, TablaSimbolos>();
 	private ArrayList<String> listaErroresSemanticos = new ArrayList<String>();
+	private ArrayList<String> operadoreslogicos = new ArrayList<>(), operadoresaritmeticos = new ArrayList<>();
 	/*
 	 * private ArrayList<String> listaErroresSemanticos; private ArrayList<String>
-	 * operadoresaritmeticos; private ArrayList<String> operadoreslogicos;
+
 	 */
 	public static void main(String[] args) {
-		String codigo = "public class m {\n" + " public int y=0; \n" + " public String y=10; \n" + " private int z; "
+		String codigo = "public class m {\n" + " public int y=0; \n" + " public boolean f=maybe; \n" + " private int z; "
 				+ "  int w=10; " + " if(y>9){\n" + "  x=8+9;\n" + " }\n" + " }\n" + "";
 		AnalisisSemantico as = new AnalisisSemantico(codigo);
 	}
@@ -20,8 +21,9 @@ public class AnalisisSemantico {
 	public AnalisisSemantico(String codigo) {
 		recorreCodigo(codigo);
 		LlenaTabla();
-		System.out.println(listaErroresSemanticos.get(0));
-		System.out.println(listaErroresSemanticos.get(1));
+		for(String error: listaErroresSemanticos) {
+			System.out.println(error);
+		}
 	}
 	public void recorreCodigo(String codigo) {
 		String parrafo = "";
@@ -57,7 +59,7 @@ public class AnalisisSemantico {
 	public void AgregaVariable(String parrafo, CharSequence Tipo, int pos) {
 		String parrafoAux = "", variable = "", valor = "";
 		CharSequence sIgual = "=", sPublic = "public", sPrivate = "private";
-		// Llenado de la declaración de las variables
+		// Llenado de la declaraciï¿½n de las variables
 		if (parrafo.contains(Tipo)) {
 			for (int j = 0; j < parrafo.length(); j++) {
 				if (parrafoAux.contains(Tipo)) {
@@ -77,6 +79,13 @@ public class AnalisisSemantico {
 						.add("La variable " + "'" + variable + "'" + " ya se encuentra declarada en el renglon "
 								+ tablaSimbolos.get(variable).getPosicion() + ".");
 			else {
+				//Verificando el valor asignado con el tipo 
+				if(!verificaTipoConValor(Tipo.toString(), valor)){
+					listaErroresSemanticos
+							.add("Imposible asignar a variable " + "'" + variable + "' Tipo (" +Tipo.toString()+ ") valor:  " + valor
+								+" renglon "	+ pos + ".");
+					return;
+				}
 				if (parrafo.contains(sPublic) || parrafo.contains(sPrivate))
 					tablaSimbolos.put(variable, new TablaSimbolos(Tipo.toString(), pos, valor, "global"));
 				else
@@ -85,23 +94,72 @@ public class AnalisisSemantico {
 		}
 
 	}
+
+	private boolean verificaTipoConValor(String tipo, String valor) {
+		if(valor.length()==0) {
+			return true;
+		}
+		switch(tipo) {
+			case "boolean":
+				return (valor.equals("true") || valor.equals("false"));
+			case "int":
+				try {
+					Integer.parseInt(valor);
+					return true;
+				}catch(Exception e) {
+					return false;
+				}
+			default:
+				return false;
+		}
+		
+	}
+
 	public void Operaciones(String parrafo, int pos) {
-		String parrafoAux = "", variable = "";
+/*		String parrafoAux = "", variable = "", operandoAux="";
+		
 		// Variables usadas y no defindas
-		// Se recorre el parrafo para obtener la variable de la operación
+		// Se recorre el parrafo para obtener la variable de la operaciï¿½n
 		for (int j = 0; j < parrafo.length(); j++) {
-			if (parrafo.charAt(j) == '=') {
+			if (parrafo.charAt(j) == '=' && parrafo.charAt(j+1)!= '=') {
 				variable = parrafoAux;
+				parrafoAux= "";
+				if (!tablaSimbolos.containsKey(variable)) {
+					listaErroresSemanticos
+							.add("La variable " + "'" + variable + "' en la posiciï¿½n " + pos + " no ha sido definida.");
 				break;
+				}
 			} else { //se eliminan los espacios en blanco
 				if (!Character.isWhitespace(parrafo.charAt(j))) {
 					parrafoAux += Character.toString(parrafo.charAt(j));
+				}else {
+					//Verificamos si es una variable
+					if(tablaSimbolos.containsKey(parrafoAux)) {//Es variable
+						if(!tablaSimbolos.get(parrafoAux).getTipoDato().equals(tablaSimbolos.get(variable).getTipoDato())) {
+							listaErroresSemanticos.add("Tipo de operacion incorrecta variable "+ variable +" Tipo ("+tablaSimbolos.get(variable).getTipoDato()+
+									") con variable "+ parrafoAux + " Tipo ( "+tablaSimbolos.get(parrafoAux)+")");
+							break;
+						}
+					}else {//Es operando u constante
+						if(verificarTipoConOperando (tablaSimbolos.get(variable), parrafoAux)) {//Es un operador
+							
+						}else {
+							
+						}
+					}
+					parrafoAux="";
 				}
 			}
+		}*/
+	}
+	
+	public boolean verificarTipoConOperando(String tipo, String operando) {
+		switch(tipo) {
+			case "int":
+				return (operando.equals("+") || operando.equals("*") || operando.equals("/") || operando.equals("-"));
+			case "boolean":
+				return (operando.equals("&&") || operando.equals("|") || operando.equals("||") || operando.equals("!=")|| operando.equals("=="));
 		}
-		if (!tablaSimbolos.containsKey(variable)) {
-			listaErroresSemanticos
-					.add("La variable " + "'" + variable + "' en la posición " + pos + " no ha sido definida.");
-		}
+		return false;
 	}
 }
